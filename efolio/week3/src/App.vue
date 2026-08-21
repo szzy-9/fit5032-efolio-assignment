c69c2c07664d64a325519b4b08bf2e6d75bf1e72
May <mxuu0084@student.monash.edu>
2026-08-21 23:42:59 +1000

    Week 3: complete CSS form styling

---
+++ efolio/week3/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+
+// import JSON from './components/JSON.vue'
+</script>
+
+<template>
+  <LibraryRegistrationForm />
+
+  <!-- <JSON /> -->
+</template>
